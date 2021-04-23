ee894a86ad88093a002cf9f146d3305b6ab08410
Eugene <yujian.wu1@gmail.com>
2021-04-23 08:26:29 +0000

    cleanup

--- tdp/bye.sh
+++
@@ -1,2 +0,0 @@
-clang bye.c -lbpf
-LD_PRELOAD="/home/simplekv/libbpf-boot/libbpf/src/libbpf.so.0" ./a.out
--- tdp/run.sh
+++
@@ -1,2 +0,0 @@
-clang test.c -lbpf
-LD_PRELOAD="/home/simplekv/libbpf-boot/libbpf/src/libbpf.so.0" ./a.out
