20cc9dd3fdc65ef45b7a1243886c7089cda5ac4a
Evan Mesterhazy <etm2131@columbia.edu>
2021-11-19 18:22:23 -0800

    simple_retrieve: Add cli flag -x to enable BPF lookup

--- simple_retrieve.c
+++ simple_retrieve.c
@@ -6,8 +6,6 @@
  * Usage:
  * ./simple_retrieve database_file key
  *
- *
- * TODO: Add a flag to use XRP to traverse the tree
  */
 
 #include <stdio.h>
@@ -18,7 +16,7 @@
 #include <ctype.h>
 #include <string.h>
 
-#define USE_BPF
+#define SYS_IMPOSTER_PREAD64 445
 
 #include "db.h"
 
@@ -72,36 +70,7 @@ void read_value_the_hard_way(int fd, char *retval, ptr__t ptr) {
     memcpy(retval, ((char *) &buf + offset), sizeof(val__t));
 }
 
-/**
- * Traverses the B+ tree index and retrieves the value associated with
- * [key] from the heap, if [key] is in the database.
- * @param file_name
- * @param key
- * @return null terminated string containing the value on disk, or NULL if key not found
- */
-char *grab_value(char *file_name, unsigned long const key) {
-    char *const retval = malloc(sizeof(val__t) + 1);
-    if (retval == NULL) {
-        perror("malloc");
-        exit(1);
-    }
-    /* Ensure we have a null at the end of the string */
-    retval[sizeof(val__t)] = '\0';
-
-    /* Open the database */
-    // TODO (etm): This is never closed
-    int flags = O_RDONLY;
-#ifdef USE_BPF
-    flags = flags | O_DIRECT;
-#endif
-    int db_fd = open(file_name, flags);
-    if (db_fd < 0) {
-        perror("failed to open database");
-        exit(1);
-    }
-
-#ifdef USE_BPF
-#define SYS_IMPOSTER_PREAD64 445
+void lookup_bpf(int db_fd, key__t key, char *retval) {
     /* Set up buffers and query */
     char *buf = aligned_alloc(0x1000, 0x1000);
     char *scratch = aligned_alloc(0x1000, 0x1000);
@@ -138,40 +107,70 @@ char *grab_value(char *file_name, unsigned long const key) {
     free(buf);
     free(scratch);
     close(logfile);
-#else
-    /* Traverse b+ tree index in db to find value */
-    Node *const node;
-    if (posix_memalign((void **) &node, 512, sizeof(Node))) {
-        perror("posix_memalign failed");
+}
+
+/**
+ * Traverses the B+ tree index and retrieves the value associated with
+ * [key] from the heap, if [key] is in the database.
+ * @param file_name
+ * @param key
+ * @return null terminated string containing the value on disk, or NULL if key not found
+ */
+char *grab_value(char *file_name, unsigned long const key, int use_xrp) {
+    char *const retval = malloc(sizeof(val__t) + 1);
+    if (retval == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+    /* Ensure we have a null at the end of the string */
+    retval[sizeof(val__t)] = '\0';
+
+    /* Open the database */
+    // TODO (etm): This is never closed
+    int flags = O_RDONLY;
+    if (use_xrp) {
+    flags = flags | O_DIRECT;
+    }
+    int db_fd = open(file_name, flags);
+    if (db_fd < 0) {
+        perror("failed to open database");
         exit(1);
     }
 
-    checked_pread(db_fd, node, sizeof(Node), 0);
-    ptr__t ptr = nxt_node(key, node);
-    while (node->type != LEAF) {
-        checked_pread(db_fd, node, sizeof(Node), (long) decode(ptr));
-        ptr = nxt_node(key, node);
-    }
-    /* Verify that the key exists in this leaf node, otherwise missing key */
-    if (!key_exists(key, node)) {
+    if (use_xrp) {
+        lookup_bpf(db_fd, key, retval);
+    } else {
+        /* Traverse b+ tree index in db to find value */
+        Node *const node;
+        if (posix_memalign((void **) &node, 512, sizeof(Node))) {
+            perror("posix_memalign failed");
+            exit(1);
+        }
+
+        checked_pread(db_fd, node, sizeof(Node), 0);
+        ptr__t ptr = nxt_node(key, node);
+        while (node->type != LEAF) {
+            checked_pread(db_fd, node, sizeof(Node), (long) decode(ptr));
+            ptr = nxt_node(key, node);
+        }
+        /* Verify that the key exists in this leaf node, otherwise missing key */
+        if (!key_exists(key, node)) {
+            free(node);
+            free(retval);
+            close(db_fd);
+            return NULL;
+        }
+
+        /* Now we're at a leaf node, and ptr points to the log entry */
+        read_value_the_hard_way(db_fd, retval, ptr);
         free(node);
-        free(retval);
-        close(db_fd);
-        return NULL;
     }
-
-    /* Now we're at a leaf node, and ptr points to the log entry */
-//    checked_pread(db_fd, retval, sizeof(val__t), (long) decode(ptr));
-    read_value_the_hard_way(db_fd, retval, ptr);
-    free(node);
     close(db_fd);
-#endif
-
     return retval;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 3 && argc != 4) {
         fprintf(stderr, "%s file key", *argv);
     }
 
@@ -184,7 +183,17 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    char *val = grab_value(file_name, key);
+    /* Check if we should use XRP for the read or not */
+    int use_xrp = 0;
+    char c;
+    while ((c = getopt(argc, argv, "x")) != -1) {
+        if (c == 'x') {
+            use_xrp = 1;
+            break;
+        }
+    }
+
+    char *val = grab_value(file_name, key, use_xrp);
     if (val == NULL) {
         printf("value not found\n");
         return 2;
